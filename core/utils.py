c623c3540304019b8229907f9a4323303685f522
WakelessSloth56 <wsloth56@foxmail.com>
2022-10-02 21:53:23 +0800

    feat(core.utils): writeBinary

--- core/utils.py
+++ core/utils.py
@@ -12,16 +12,21 @@ if __name__ == "__main__":
     die()
 
 
+def getFileToWrite(file: str) -> str:
+    absFile = os.path.abspath(file)
+    absDir = os.path.dirname(absFile)
+    if not os.path.exists(absDir):
+        os.makedirs(absDir)
+    return absFile
+
+
 def readText(file: str):
     with open(file, "r", encoding="utf-8") as f:
         return f.read()
 
 
 def writeText(file: str, text: str) -> str:
-    absFile = os.path.abspath(file)
-    absDir = os.path.dirname(absFile)
-    if not os.path.exists(absDir):
-        os.makedirs(absDir)
+    absFile = getFileToWrite(file)
     with open(absFile, "w", encoding="utf-8") as f:
         f.write(text)
     return absFile
@@ -33,3 +38,10 @@ def readJson(file: str):
 
 def writeJson(file: str, jsonObj: dict) -> str:
     return writeText(file, json.dumps(jsonObj, ensure_ascii=False, indent=4))
+
+
+def writeBinary(file: str, buffer) -> str:
+    absFile = getFileToWrite(file)
+    with open(absFile, "wb") as f:
+        f.write(buffer)
+    return absFile
